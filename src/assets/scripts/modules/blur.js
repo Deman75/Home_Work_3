b1e9491e6d398ba1c3b4c7fffb923369ecc715b3
Dmitriy Shapiro <deman75@yandex.ru>
2018-10-07 21:16:18 +0300

    Blur add

---
+++ src/assets/scripts/modules/blur.js
@@ -0,0 +1,28 @@
+console.log('blur is connect');
+
+const section = document.querySelector('#section__feedback');
+const blur = document.querySelector('#feedback__blur');
+const width = 1800;
+const height = 1699;
+var   divW = section.offsetWidth;
+var   divH = section.offsetHeight;
+var finalWidth;
+
+const changeBlurBgSize = () => {
+  divW = section.offsetWidth;
+  divH = section.offsetHeight;
+
+  if ((divW/divH)<(width/height)) {
+    finalWidth = (divH * (width/height)) + (divW * 0.1);
+  } else {
+    finalWidth = divW;
+  }
+
+  blur.style = `background-size: ${finalWidth}px`;
+}
+
+changeBlurBgSize();
+
+window.onresize = () => {
+  changeBlurBgSize();
+};
